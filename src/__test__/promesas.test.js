91b5a6ad28e948c4c945744fab850a9993ddc052
efrencodes <efrencodes@gmail.com>
2022-06-22 18:06:27 -0500

    :test_tube: Add test

--- src/__test__/promesas.test.js
+++ src/__test__/promesas.test.js
@@ -15,9 +15,16 @@ describe("Implementando pruebas a promesas", () => {
     const string = await reverseString("HOLA");
     expect(string).toBe("ALOH");
   });
+  it("Falla la promesa", async () => {
+    try {
+      await reverseString();
+    } catch (e) {
+      expect(e.message).toEqual("Value invalid");
+    }
+  });
 });
 
-afterEach(() => console.log("Después de cada prueba"));
-afterAll(() => console.log("Después de todas las pruebas"));
-beforeEach(() => console.log("Antes de cada prueba"));
-beforeAll(() => console.log("Antes de todas las pruebas"));
+// afterEach(() => console.log("Después de cada prueba"));
+// afterAll(() => console.log("Después de todas las pruebas"));
+// beforeEach(() => console.log("Antes de cada prueba"));
+// beforeAll(() => console.log("Antes de todas las pruebas"));

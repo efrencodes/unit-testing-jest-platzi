5f3aded73ec2af8d5a2d542ea4b8dccd60d0f03c
efrencodes <efrencodes@gmail.com>
2022-07-04 17:35:22 -0500

    :white_check_mark: Add test Footer

---
+++ platzi-store/src/__test__/components/Footer.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+import { mount } from 'enzyme';
+
+describe('Componente <Footer />', () => {
+  const footer = mount(<Footer />);
+  test('Render del componente Footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+  test('Render del copy', () => {
+    expect(footer.find('.Footer-copy').text()).toEqual(
+      'Todos los Izquierdos Reservados'
+    );
+  });
+});
